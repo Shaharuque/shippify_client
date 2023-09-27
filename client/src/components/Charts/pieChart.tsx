8f685ad8fbebb9dd04aff1fb4c1ecbc0d26f04f3
Zaber Ahmed <zaber0305@gmail.com>
2023-09-27 21:08:22 +0600

    Add: pie chart data from backend

--- client/src/components/Charts/pieChart.tsx
+++ client/src/components/Charts/pieChart.tsx
@@ -1,8 +1,32 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import ReactApexChart from 'react-apexcharts';
 
 const PieChart = () => {
+	const [labels, setLabels] = useState<string[]>([]);
+	const [values, setValues] = useState<number[]>([]);
+
+	useEffect(() => {
+		const token = localStorage.getItem('token');
+		const fetchPieChartData = async () => {
+			try {
+				const result = await axios.get('http://192.168.68.89:5000/shipment/basic/pie/chart/group/by/shipping/status', {
+					headers: {
+						'Content-Type': 'application/json',
+						'x-auth-token': token,
+					},
+				});
+				console.log('result:', result.data);
+				setLabels(result?.data?.data.map((item: { count: number; status: string }) => item.status));
+				setValues(result?.data?.data.map((item: { count: number; status: string }) => item.count));
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchPieChartData();
+	}, []);
 	const options = {
-		labels: ['Total Shipments', 'Active', 'Returned'],
+		labels: labels,
 		legend: { position: 'right' as 'right' },
 
 		responsive: [
@@ -15,10 +39,19 @@ const PieChart = () => {
 					legend: {
 						position: 'right' as 'right',
 						fontSize: '16px',
-
+						formatter: function (val, opts) {
+							const formattedLabel = val.split('_').join(' ');
+							return `
+        <div style="display: flex; align-items: center;">
+          <div style="background-color: ${opts.w.config.colors[opts.seriesIndex]}; width: 10px; height: 10px; margin-right: 5px;"></div>
+          <div>${formattedLabel}</div>
+        </div>
+      `;
+						},
+						height: 300,
 						itemMargin: {
 							horizontal: 0,
-							vertical: 10,
+							vertical: 0,
 						},
 					},
 				},
@@ -31,6 +64,7 @@ const PieChart = () => {
 					},
 					legend: {
 						position: 'bottom' as 'bottom',
+
 						itemMargin: {
 							horizontal: 10,
 							vertical: 0,
@@ -40,7 +74,7 @@ const PieChart = () => {
 			},
 		],
 	};
-	const series = [14, 9, 5];
+	const series = values;
 	return (
 		<>
 			<ReactApexChart
