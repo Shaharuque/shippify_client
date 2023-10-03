b1d66039ae11b013bfe4bf75d0d8e5d2369b8cae
fiona <shahrukhamin27@gmail.com>
2023-10-03 19:30:25 +0600

    updated

--- client/src/pages/Tracking/trackingPage.tsx
+++ client/src/pages/Tracking/trackingPage.tsx
@@ -1,10 +1,26 @@
 import TrackingList from '../../components/Tracking/trackingList';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 
 const TrackingPage = () => {
 	return (
-		<>
-			<TrackingList />
-		</>
+		<div className='mt-10'>
+			<Tabs
+				isFitted
+				variant="soft-rounded">
+				<TabList px={'80px'}>
+					<Tab _selected={{ color: 'white', bg: 'cta' }}>Basic Shipments (Tracking)</Tab>
+					<Tab _selected={{ color: 'white', bg: 'cta' }}>LTL Shipments(Tracking)</Tab>
+				</TabList>
+				<TabPanels>
+					<TabPanel>
+						<TrackingList />
+					</TabPanel>
+					<TabPanel>
+						<p>two!</p>
+					</TabPanel>
+				</TabPanels>
+			</Tabs>
+		</div>
 	);
 };
 
